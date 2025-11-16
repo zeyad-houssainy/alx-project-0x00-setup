b6e6695b18dc9281d0d6808c4b3ea7a93ea54413
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-11-17 01:08:18 +0400

    2. Basic Routing

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -3,7 +3,9 @@ const Home: React.FC = () => {
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
       <h1>Airbnb Application Clone system</h1>
       <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+      <p>This file is index.ts inside the pages</p>
     </main>
+
   )
 }
 
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,9 @@
+const Landing: React.FC =  () => {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <p>Welcome to the landing page</p>
+    </div>
+  )
+}
+export default Landing
